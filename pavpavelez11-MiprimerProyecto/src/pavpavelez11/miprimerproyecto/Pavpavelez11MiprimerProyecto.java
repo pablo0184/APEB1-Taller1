5a927d9d9a957a50d4db86cb5fc1a03eceaa166c
pablo <pablo.velezleon1@icloud.com>
2023-10-19 17:08:26 -0500

    este es mi primer programa que subo de clase

---
+++ pavpavelez11-MiprimerProyecto/src/pavpavelez11/miprimerproyecto/Pavpavelez11MiprimerProyecto.java
@@ -0,0 +1,9 @@
+
+
+package pavpavelez11.miprimerproyecto;
+public class Pavpavelez11MiprimerProyecto {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+    
+}
