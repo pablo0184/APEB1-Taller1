cda4860503875312e6e41c5807d1a3abb39085c8
pablo <pablo.velezleon1@icloud.com>
2023-10-23 23:25:55 -0500

    segundo commit

--- pavpavelez11-MiprimerProyecto/proyecto 1 pablo/src/proyecto/pkg1/pablo/Proyecto1Pablo.java
+++ pavpavelez11-MiprimerProyecto/proyecto 1 pablo/src/proyecto/pkg1/pablo/Proyecto1Pablo.java
@@ -14,6 +14,7 @@ public class Proyecto1Pablo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("hola mundo");
         // TODO code application logic here
     }
     
